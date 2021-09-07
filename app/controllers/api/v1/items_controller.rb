3d24a5494d19c3a6536400295ef91006a6cfb965
Brett Jones <bhjones4545@gmail.com>
2021-09-07 15:00:38 -0600

    Add index action to items controller

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::ItemsController < ApplicationController
+  def index
+    if params[:page].nil?
+      page = params.fetch(:page, 1).to_i
+    elsif params[:page].to_i <= 0
+      page = 1
+    else
+      page = params[:page].to_i
+    end
+
+    if params[:per_page].nil?
+      limit = 20
+    else
+      limit = params[:per_page].to_i
+    end
+
+    items = Item.offset((page - 1) * limit).limit(limit)
+    render json: ItemSerializer.new(items)
+  end
+end
