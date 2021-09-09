c6b058f623037dbe39cc74d5b7bd47b4b9e8f807
Brett Jones <bhjones4545@gmail.com>
2021-09-09 13:26:32 -0600

    Add model methods for top revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,21 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+
+  def self.merchant_revenue(merchant_id)
+    self.joins(:transactions)
+    .joins(:invoice_items)
+    .joins(:invoices)
+    .select("merchants.id as id, merchants.name as name, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+    .where("merchants.id = ?", "#{merchant_id}")
+    .group(:name, :id)
+  end
+
+  def self.top_revenue
+    joins(:transactions, :invoice_items)
+    .select("merchants.id as id, merchants.name as name, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+    .group(:name, :id)
+    .order(revenue: :desc)
+    .where("invoices.status = ? and transactions.result = ?", 'shipped', 'success')
+  end
 end
