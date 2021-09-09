e3c76d744a2aba360813605e7275dc3cfe9e9cfa
Brett Jones <bhjones4545@gmail.com>
2021-09-09 15:45:00 -0600

    refactor to green postman tests

--- app/serializers/merchant_total_revenue_serializer.rb
+++ app/serializers/merchant_total_revenue_serializer.rb
@@ -1,6 +1,7 @@
 class MerchantTotalRevenueSerializer
   include JSONAPI::Serializer
+  attributes :name
 
-  set_type "merchant_revenue"
+  set_type "merchant_name_revenue"
   attributes :revenue
 end
